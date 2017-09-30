0227c7ef2ab7317292794365c9e7a27f0c9ef63a
kang77649119 <77649119@qq.com>
2017-09-30 13:41:07 +0800

    tip 完善

--- Swift4JsonDemo/Swift4JsonDemo/Classes/Main/HorseModel.swift
+++ Swift4JsonDemo/Swift4JsonDemo/Classes/Main/HorseModel.swift
@@ -17,6 +17,9 @@ class HorseModel: NSObject, Codable {
     var courtName:String?
     var imageUrls:[String]?
     
+    // 字典中存在的属性,但我不去赋值
+    var location:String?
+    
     // 嵌套属性
     var flatOptions:[HorseType]?
     
@@ -28,12 +31,15 @@ class HorseModel: NSObject, Codable {
     
     /**
      tip1:
-         如果模型中的属性不包含自定义属性,可以不用实现 required init(from: Decoder) 直接转模型就可以
-         如果模型中包含了自定义属性,必须实现 required init(from: Decoder) 并且在内部对每个属性进行取值赋值,自定义属性根据需要可以选择赋值也可以不赋值
+         如果模型中的属性不包含自定义属性，只有字典中的属性(不是一一对应)，这时转模型就可以不用实现 required init(from: Decoder) ，直接转模型就可以
+     
+         如果模型中包含了自定义属性，必须实现 required init(from: Decoder) 并且在内部对每个属性进行取值赋值,自定义属性根据需要可以选择赋值也可以不赋值
      
      tip2:
-         如果返回的字典中的字段属性不符合模型中的属性规范,可以在 CodingKeys 中设置该字段的映射名称
+         如果模型中的属性与字典中的字典是一一对应关系，并且不包含自定义属性，那么模型中除了要实现Codable协议外，不需要写任何代码，直接进行转模型就可以
      
+     tip3:
+         如果返回的字典中的字段属性与模型中的属性规范不同,可以在 CodingKeys 中对该字段进行名称映射
      */
     
     required init(from: Decoder) {
@@ -50,6 +56,7 @@ class HorseModel: NSObject, Codable {
 
     }
 
+    // 定义需要解码的属性
     enum CodingKeys:String, CodingKey {
         case address
         case city
@@ -59,8 +66,7 @@ class HorseModel: NSObject, Codable {
         case horsePoint = "geoPoint"
         case flatOptions
     }
-
-
+    
 }
 
 
